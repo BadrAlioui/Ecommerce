98f859fd853e7e21cd824db20e475959dd42371b
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-02 19:54:28 +0000

    Put back the form, search functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ import cloudinary.api
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 
