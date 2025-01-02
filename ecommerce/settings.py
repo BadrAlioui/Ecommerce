5abfd836de34c347be3d57696f00266bbcadb7cd
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-02 19:57:27 +0000

    Reduce the size of the search bar

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ import cloudinary.api
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
